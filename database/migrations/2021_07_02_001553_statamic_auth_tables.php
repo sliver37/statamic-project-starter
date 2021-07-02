dc9daeca1359b9dfda2cc2958967906cf591d54c
Michael Rook <michael@handmadeweb.com.au>
2021-07-02 10:55:20 +1000

    Add indexes and adjust existing columns

--- database/migrations/2021_07_02_001553_statamic_auth_tables.php
+++ database/migrations/2021_07_02_001553_statamic_auth_tables.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class StatamicAuthTables extends Migration
 {
@@ -14,40 +14,44 @@ class StatamicAuthTables extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('super')->default(false);
+            // Add new columns
+            $table->boolean('super')->default(false)->index();
             $table->string('avatar')->nullable();
             $table->json('preferences')->nullable();
             $table->timestamp('last_login')->nullable();
+
+            // Update existing columns
+            $table->string('id')->change();
             $table->string('password')->nullable()->change();
         });
 
         Schema::create('role_user', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
-            $table->string('role_id');
+            $table->string('user_id')->index();
+            $table->string('role_id')->index();
         });
 
         Schema::create('group_user', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
-            $table->string('group_id');
+            $table->string('user_id')->index();
+            $table->string('group_id')->index();
         });
     }
 
     /**
      * Reverse the migrations.
      */
-     public function down()
-     {
-         Schema::table('users', function (Blueprint $table) {
-             $table->dropColumn('super');
-             $table->dropColumn('avatar');
-             $table->dropColumn('preferences');
-             $table->dropColumn('last_login');
-             $table->string('password')->nullable(false)->change();
-         });
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('super');
+            $table->dropColumn('avatar');
+            $table->dropColumn('preferences');
+            $table->dropColumn('last_login');
+            $table->string('password')->nullable(false)->change();
+        });
 
-         Schema::dropIfExists('role_user');
-         Schema::dropIfExists('group_user');
-     }
+        Schema::dropIfExists('role_user');
+        Schema::dropIfExists('group_user');
+    }
 }
