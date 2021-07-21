7c3ebabf02bcc55a535c5daa9f620c66601bfee1
Michael Rook <michael@rook.net.au>
2021-07-21 19:26:22 +1000

    Add stacks and div ID app.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,11 +6,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ $title ?? $site->name() }}</title>
         <link rel="stylesheet" href="{{ mix('css/tailwind.css') }}">
+
+        @stack('styles')
     </head>
     <body class="bg-gray-100 font-sans leading-normal text-grey-800">
-        <div class="mx-auto px-2 h-screen flex items-center justify-center">
+        <div id="app" class="mx-auto px-2 h-screen flex items-center justify-center">
             @yield('template_content')
         </div>
         <script src="{{ mix('js/site.js') }}"></script>
+
+        @stack('scripts')
     </body>
 </html>
