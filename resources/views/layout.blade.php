b8512871f34a9426a8bbe5ae95036ea02cef51a7
Michael Rook <michael@rook.net.au>
2021-07-21 19:27:03 +1000

    Add csrf

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ $csrf_token }}" />
         <title>{{ $title ?? $site->name() }}</title>
+        
         <link rel="stylesheet" href="{{ mix('css/tailwind.css') }}">
 
         @stack('styles')
